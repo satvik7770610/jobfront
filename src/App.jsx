bba34aa5f294d6c3c4e638cfe78d04fda899cc05
satvik7770610 <ravikiran32456@gmail.com>
2025-09-27 08:15:09 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const API_BASE_URL = 'http://localhost:5432';
+const API_BASE_URL = 'http://localhost:8081';
 
 // A simple message box component for displaying feedback
 const MessageBox = ({ message, type }) => {
